65c01fe187de6b49d00a9f162dd804785c7cb877
ValCam18 <valeria-camila18@hotmail.com>
2024-12-09 19:14:43 -0500

    Primer commit

---
+++ arreglo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int array[] ={0,1,2,3,4,5,6};
+
+    cout<<"El arreglo es:"<<endl;
+    for(int i = 0; i< 7; i++) {
+        cout<<array[i]<<endl;
+
+    }
+
+
+    return 0;
+}
